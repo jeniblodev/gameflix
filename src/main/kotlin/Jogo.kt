079265eaac4172f89d08902b2f5981a95709763e
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-03-08 14:08:59 -0300

    Initial commit

---
+++ src/main/kotlin/Jogo.kt
@@ -0,0 +1,8 @@
+class Jogo {
+    var titulo = ""
+    var descricao = ""
+    var preco = 0
+    var loja = ""
+
+
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val jogo1 = Jogo()
+
+    jogo1.titulo = "Batman: Arkham Asylum Game of the Year Edition"
+    print(jogo1.titulo)
+}
