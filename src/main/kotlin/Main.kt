727a16a3d84a0493d17017d5d27b2c14c16f7cc0
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-03-10 17:05:04 -0300

    Adições desse commit: dados da api sendo utilizados no projeto, inclusão das classes de dados, alteração dos atributos da classe Jogo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,33 @@
-fun main() {
-    val jogo1 = Jogo()
+import com.google.gson.Gson
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
+import java.util.Scanner
 
-    jogo1.titulo = "Batman: Arkham Asylum Game of the Year Edition"
-    print(jogo1.titulo)
+
+fun main() {
+    val leitura = Scanner(System.`in`)
+    println("Digite um código de jogo para buscar:")
+    val busca = leitura.nextLine()
+
+    val enderecoBusca = "https://www.cheapshark.com/api/1.0/games?id=$busca"
+
+
+    //adicionando a conexão: client faz a solicitação, request informa como, response manda o resultado
+    val client: HttpClient = HttpClient.newHttpClient()
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create(enderecoBusca))
+        .build()
+    val response = client
+        .send(request, BodyHandlers.ofString())
+    val json = response.body()
+    println(json)
+
+    val gson = Gson()
+    val info = gson.fromJson(json, Jogo::class.java)
+
+    println(info)
+
 }
+
